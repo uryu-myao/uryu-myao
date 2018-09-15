24a8001ca1707412bd4837a93c7a2253f356454d
Uryu Myao <myao.jpn@gmail.com>
2018-09-16 01:56:04 +0900

    overwriting some css iwith sass loop

--- src/script/mod/topSlider.js
+++ src/script/mod/topSlider.js
@@ -7,31 +7,39 @@
 */
 
 
-const elemCount = 3;
-const elemTrans = 0;
+(function() {
 
-var current = 1,
-    trans = 1;
+    const elemCount = 3;
+    const elemTrans = 0;
 
-const nextElem = document.querySelector('.grid__prev');
-const prevElem = document.querySelector('.grid__next');
-const currentSlider = document.querySelector('.grid__curS .__cur-show > *');
+    let current = 1,
+        trans = 1;
 
-nextElem.addEventListener('click', nextImage);
-prevElem.addEventListener('click', prevImage);
+    const nextElem = document.querySelector('.grid__prev');
+    const prevElem = document.querySelector('.grid__next');
+    const currentSlider = document.querySelector('.grid__curS .__cur-show > *');
+    const detailsBtn = document.querySelector('.__curI-desc button a');
 
-function nextImage () {
-    if ( current < elemCount )
-    {
-        current++;
-        console.log(current);
+    nextElem.addEventListener('click', nextImage);
+    prevElem.addEventListener('click', prevImage);
+
+    detailsBtn.href = "/works/01.html";
+
+    function nextImage () {
+        if ( current < elemCount )
+        {
+            current++;
+            console.log(current);
+        }
+    };
+
+    function prevImage () {
+        if ( current > 0 )
+        {
+            current--;
+            console.log(current);
+        }
     }
-};
 
-function prevImage () {
-    if ( current > 0 )
-    {
-        current--;
-        console.log(current);
-    }
-}
+
+}());
