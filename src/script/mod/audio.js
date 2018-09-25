6a259f57300eed22c54c3284d00f22e876b6da01
Uryu Myao <myao.jpn@gmail.com>
2018-09-26 01:44:55 +0900

    implement bgm and other sounds

--- src/script/mod/audio.js
+++ src/script/mod/audio.js
@@ -1,14 +1,55 @@
 (function() {
 
-    const audio__hover = document.querySelector('.audio__hv');
-    const audio = document.querySelector('#sound_hv-1');
+    // Hover Sound Resourse
+    const glitch_01 = document.querySelector('#sound_hv-1');
+    const BGM = document.querySelector('#sound_bgm');
+
+    let audio_hvGlitch_01 = document.querySelector('.audio__hv'),
+        audio_switchBGM = document.querySelector('.sound__switch');
 
 
-    audio__hover.addEventListener('mouseenter', function() {
-        audio.play();
-    }, false);
-    audio__hover.addEventListener('mouseleave', function() {
-        audio.pause();
-    }, false)
+    let btnHoverSoundEffect = function () {
+
+        audio_hvGlitch_01.addEventListener('mouseenter', function() {
+            glitch_01.play();
+        }, false);
+        audio_hvGlitch_01.addEventListener('mouseleave', function() {
+            glitch_01.pause();
+        }, false);
+
+    };
+
+    btnHoverSoundEffect();
+
+
+    let play = true;
+
+    let toggleBGMBtn = function () {
+
+        if (play) {
+
+            audio_switchBGM.classList.add('_pause');
+            BGM.pause();
+
+            play = false;
+
+        } else {
+
+            audio_switchBGM.classList.remove('_pause');
+            BGM.play();
+
+            play = true;
+
+        }
+    };
+
+    let switchBGM = function() {
+        audio_switchBGM.addEventListener('click', toggleBGMBtn, false);
+    };
+
+
+    BGM.play();
+    switchBGM();
+
 
 }());
