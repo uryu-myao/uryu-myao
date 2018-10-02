966cf80f54ce4e5f2bfd8e3561dac20fdfeefadd
Uryu Myao <myao.jpn@gmail.com>
2018-10-03 00:11:32 +0900

    changes

--- src/script/mod/preload.js
+++ src/script/mod/preload.js
@@ -1,32 +1,32 @@
 (function() {
 
-    const LOADING = (function() {
-
-        let loadingPage = document.querySelector('#preload')
-
-
-        let preload = setInterval(function() {
-
-            if (document.readyState === 'complete') {
-
-                loadingPage.classList.remove('_preload_loading');
-                clearInterval(preload);
-
-            }
-
-        }, 500);
-
-
-        let bindActions = function() {
-            window.onload = preload;
-        };
-
-        let init = function() { bindActions();};
-
-        return { init: init };
-
-    }());
-
-    LOADING.init();
+    // const LOADING = (function() {
+    //
+    //     let loadingPage = document.querySelector('#preload')
+    //
+    //
+    //     let preload = setInterval(function() {
+    //
+    //         if (document.readyState === 'complete') {
+    //
+    //             loadingPage.classList.remove('_preload_loading');
+    //             clearInterval(preload);
+    //
+    //         }
+    //
+    //     }, 500);
+    //
+    //
+    //     let bindActions = function() {
+    //         window.onload = preload;
+    //     };
+    //
+    //     let init = function() { bindActions();};
+    //
+    //     return { init: init };
+    //
+    // }());
+    //
+    // LOADING.init();
 
 }());
