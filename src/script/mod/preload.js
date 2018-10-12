02ebf517fe7588f61fbea27d3c2fc0ea194b04d3
Uryu Myao <myao.jpn@gmail.com>
2018-10-12 16:35:20 +0900

    random letters

--- src/script/mod/preload.js
+++ src/script/mod/preload.js
@@ -1,36 +1,53 @@
 (function() {
     const Loading = (function() {
+
         let preload = document.querySelector('#Preload'),
             entryBtn = document.querySelector('.entry'),
-            entryLetter = ['.e1','.e2','.e3','.e4','.e5']
+            entryLetters = ['.e1','.e2','.e3','.e4','.e5'];
 
         let bindLoadingAnimation = function() {
+            // Cyberline motion in CSS
             preload.classList.remove('loading');
             preload.classList.add('loaded');
 
+            // Letters fade in
             TweenMax.delayedCall( 1, letterAimation );
-
             function letterAimation() {
-                TweenMax.staggerFromTo( entryLetter, .4,
+                TweenMax.staggerFromTo( entryLetters, .4,
                     { opacity: 0 },
                     { opacity: 1 }, .2
                 )
             }
         };
 
-        let entringAnimaion = function () {
+        let bindEntringAnimaion = function () {
+            let entryLetterNodes = document.querySelectorAll('.enl'),
+                arrEntryLetters = Array.from( entryLetterNodes );
 
-            TweenMax.to( entryBtn, .2,
-                { ease: Circ.easeOut, opacity: 0 }
+            // Letters fade out randomly
+            TweenMax.set( arrEntryLetters, { opacity: 1 });
+
+            arrEntryLetters.sort( function() {
+                return .3 - Math.random()
+            });
+
+            TweenMax.staggerTo( arrEntryLetters, .4,
+                { opacity: 0, ease: Quad.easeInOut }, .2
             );
-            TweenMax.to( preload, .2,
-                { ease: Circ.easeOut, opacity: 0 }
-            );
+
+            // Whole Preload fade out
+            TweenMax.delayedCall( .8, bgFadeOut );
+
+            function bgFadeOut() {
+                TweenMax.to( preload, .5,
+                    { ease: Circ.easeOut, opacity: 0 }
+                )
+            }
         }
 
         let bindActions = function() {
             document.addEventListener("DOMContentLoaded", bindLoadingAnimation);
-            if (entryBtn) { entryBtn.addEventListener("click", entringAnimaion); }
+            if (entryBtn) { entryBtn.addEventListener("click", bindEntringAnimaion); }
         };
 
         let init = function() {
