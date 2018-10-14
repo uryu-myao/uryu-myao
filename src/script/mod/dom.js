c8f79d09684fe7993ed537f8e0d08f87f21aa06d
Uryu Myao <myao.jpn@gmail.com>
2018-10-14 18:56:23 +0900

    top slider

--- src/script/mod/dom.js
+++ src/script/mod/dom.js
@@ -9,30 +9,30 @@
 
 (function(){
 
-    let top = document.querySelector('#Top')
-
-    if (top) {
-        let dataJSON = new XMLHttpRequest(),
-            url = "/data/data.json"
-
-        dataJSON.open("GET", url, true);
-        dataJSON.setRequestHeader('content-type', 'application/json');
-
-        dataJSON.onreadystatechange = function() {
-          if ((dataJSON.readyState === 4) && (dataJSON.status === 200)) {
-              let dataContent = JSON.parse(dataJSON.responseText),
-                  banner_motion_prev = document.querySelector('.grid_prev p'),
-                  banner_motion_next = document.querySelector('.grid_next p')
-
-              banner_motion_prev.innerHTML = dataContent.prev[0].title.repeat(15);
-              banner_motion_next.innerHTML = dataContent.next[0].title.repeat(16)
-          }
-        }
-
-        dataJSON.send(null)
-
-    } else {
-        return false
-    }
+    // let top = document.querySelector('#Top')
+    //
+    // if (top) {
+    //     let dataJSON = new XMLHttpRequest(),
+    //         url = "/data/data.json"
+    //
+    //     dataJSON.open("GET", url, true);
+    //     dataJSON.setRequestHeader('content-type', 'application/json');
+    //
+    //     dataJSON.onreadystatechange = function() {
+    //       if ((dataJSON.readyState === 4) && (dataJSON.status === 200)) {
+    //           let dataContent = JSON.parse(dataJSON.responseText),
+    //               banner_motion_prev = document.querySelector('.grid_prev p'),
+    //               banner_motion_next = document.querySelector('.grid_next p')
+    //
+    //           banner_motion_prev.innerHTML = dataContent.prev[0].title.repeat(15);
+    //           banner_motion_next.innerHTML = dataContent.next[0].title.repeat(16)
+    //       }
+    //     }
+    //
+    //     dataJSON.send(null)
+    //
+    // } else {
+    //     return false
+    // }
 
 })();
--- src/script/mod/topSlider.js
+++ src/script/mod/topSlider.js
@@ -9,65 +9,106 @@
 
 (function() {
 
-    const topSlider = (function() {
+    // const topSlider = (function() {
 
-        let elemCount = 3,
-            elemTrans = 0;
-
-        let current = 0,
-            trans = 1;
-
-        let nextElem = document.querySelector('.grid_prev'),
-            prevElem = document.querySelector('.grid_next'),
-            currentSlider = document.querySelector('.grid_thum .grid_thum_fig > *'),
-            detailsBtn = document.querySelector('.grid_desc_down button a');
-
-        nextElem.addEventListener('click', nextImage);
-        prevElem.addEventListener('click', prevImage);
-
-        detailsBtn.href = "/works/01.html";
-
-        function nextImage() {
-            if (current < elemCount){
-                current ++;
-                console.log(current);
-            }
-        };
-
-        function prevImage() {
-            if (current > 0) {
-                current --;
-                console.log(current);
-            }
-        }
-
+        // let elemCount = 3,
+        //     elemTrans = 0;
+        //
+        // let current = 0,
+        //     trans = 1;
+        //
+        // let nextElem = document.querySelector('.grid_prev'),
+        //     prevElem = document.querySelector('.grid_next'),
+        //     currentSlider = document.querySelector('.grid_thum .grid_thum_fig > *'),
+        //     detailsBtn = document.querySelector('.grid_desc_down button a');
+        //
+        // nextElem.addEventListener('click', nextImage);
+        // prevElem.addEventListener('click', prevImage);
+        //
+        // detailsBtn.href = "/works/01.html";
+        //
+        // function nextImage() {
+        //     if (current < elemCount){
+        //         current ++;
+        //         console.log(current);
+        //     }
+        // };
+        //
+        // function prevImage() {
+        //     if (current > 0) {
+        //         current --;
+        //         console.log(current);
+        //     }
+        // }
 
 
 ///   sample
 
-  
+    function slider(item) {
+        let items = item.querySelectorAll('li'),
+            current = 0,
+            autoUpdate = true,
+            timeTrans = 4000;
 
-///
+        //BTN link
+        let detailsBtn = document.querySelector('.grid_desc_down button a');
+        detailsBtn.href = "/works/01.html";
+
+        //create button prev
+        let prevbtn = document.querySelector('.grid_prev');
+
+        //create button next
+        let nextbtn = document.querySelector('.grid_next');
 
 
+        items[current].className = "current";
+        if (items) items[items.length - 1].className = "prev_slide";
 
 
+        let navigate = function(dir) {
+            items[current].className = "";
 
-
-        let bindActions = function() {
-            if ( top ) {
-
+            if (dir === 'down') {
+                current = current < items.length - 1 ? current + 1 : 0;
+            } else {
+                current = current > 0 ? current - 1 : items.length - 1;
             }
-        };
 
-        let init = function() {
-            bindActions()
-        };
+            let	nextCurrent = current < items.length - 1 ? current + 1 : 0,
+                prevCurrent = current > 0 ? current - 1 : items.length-1;
 
-        return { init: init }
+            items[current].className = "current";
+            items[prevCurrent].className = "prev_slide";
+            items[nextCurrent].className = "";
+        }
 
-    }());
+        item.addEventListener('mouseenter', function() { autoUpdate = false;});
+        item.addEventListener('mouseleave', function() { autoUpdate = true;});
 
-    topSlider.init()
+        setInterval(function() {
+            if (autoUpdate) navigate('down');
+        },timeTrans);
+
+        prevbtn.addEventListener('click', function() {
+            navigate('up');
+        });
+
+        nextbtn.addEventListener('click', function() {
+            navigate('down');
+        });
+    }
+
+
+
+    let bindActions = function() {
+        if ( top ) {
+            [].slice.call(document.querySelectorAll('.grid_thum'))
+              .forEach( function(item) {
+                slider(item);
+            });
+        }
+    };
+
+    bindActions()
 
 }());
