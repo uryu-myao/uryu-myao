5dde375d0d26f64a38dc632a892af0c2a19ff15a
Uryu Myao <myao.jpn@gmail.com>
2018-10-25 18:56:37 +0900

    modify top slider

--- src/script/mod/topSlider.js
+++ src/script/mod/topSlider.js
@@ -13,19 +13,18 @@
         let items = item.querySelectorAll('li'),
             current = 0,
             autoUpdate = true,
-            timeTrans = 5000,
-            slidersNum = 4;
+            timeTrans = 5000
 
         // Define main visual
-        let thm = document.querySelector('.grid_thum');
+        let thm = document.querySelector('.grid_thum'),
         // Define button prev
-        let prevbtn = document.querySelector('.grid_prev');
+            prevbtn = document.querySelector('.grid_prev'),
         // Define button next
-        let nextbtn = document.querySelector('.grid_next');
-
+            nextbtn = document.querySelector('.grid_next')
 
         // Set up current
         items[current].className = "current";
+
         if (items) items[items.length - 1].className = "prev_slide";
 
         let navigate = function(dir) {
@@ -51,15 +50,13 @@
 
 
         // Change BTN link
-        let detailsBtn = document.querySelector('.grid_desc_down button a');
-
-        let linkHref = function(current) {
-            detailsBtn.href = "/works/w0" + current + ".html";
-        };
-
-        setInterval( function() {
-            if ( autoUpdate ) linkHref( current + 1 );
-        }, timeTrans );
+        // let detailsBtn = document.querySelector('.grid_desc_down button a')
+        // let linkHref = function(current) {
+        //     detailsBtn.href = "/works/w0" + current + ".html";
+        // };
+        // setInterval( function() {
+        //     if ( autoUpdate ) linkHref( current + 1 );
+        // }, timeTrans );
 
 
         // Pause slider by hover-on
@@ -75,11 +72,11 @@
 
         // Execute prev and next clicking
         prevbtn.addEventListener('click', function() {
-            navigate('up')
+            navigate('up');
         });
 
         nextbtn.addEventListener('click', function() {
-            navigate('down')
+            navigate('down');
         });
     };
 
@@ -91,6 +88,10 @@
                   slider(item)
             });
             [].slice.call(document.querySelectorAll('.grid_desc_up'))
+              .forEach( function(item) {
+                  slider(item)
+            });
+            [].slice.call(document.querySelectorAll('.grid_desc_down'))
               .forEach( function(item) {
                   slider(item)
             });
