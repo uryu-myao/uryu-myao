513312f97165c7c1e770d25205ac09a91bdb5a6d
Uryu Myao <myao.jpn@gmail.com>
2018-11-08 01:19:57 +0900

    changed to muting BGM

--- src/script/mod/audio.js
+++ src/script/mod/audio.js
@@ -4,43 +4,47 @@
     const BGM = document.querySelector('#sound_bgm'); // @audio.njk
 
     // DOM
-    let audio_hvGlitch_01 = document.querySelector('.audio_hv_01'),
+    let audio_hvGlitch_01 = document.querySelectorAll('.audio_hv_01'),
         audio_switchBGM = document.querySelector('.BGM')
 
     let btnHoverSoundEffect = function() {
-        audio_hvGlitch_01.addEventListener('mouseenter', function() {
-            glitch_01.play();
-        }, false);
+        
+        for (let i = 0; i < audio_hvGlitch_01.length; i++) {
+            audio_hvGlitch_01[i].addEventListener('mouseenter', function () {
+                glitch_01.play()
+            }, false);
 
-        audio_hvGlitch_01.addEventListener('mouseleave', function() {
-            glitch_01.pause();
-        }, false)
-    };
+            audio_hvGlitch_01[i].addEventListener('mouseleave', function () {
+                glitch_01.pause()
+            }, false)
+        }
+    }
 
     if (audio_hvGlitch_01) {
         btnHoverSoundEffect()
     }
 
     // Turn on/off BGM
-    let play = true;
+    let play = false;
 
     let toggleBGMBtn = function () {
-        if (play) {
-            audio_switchBGM.classList.add('_pause');
-            BGM.pause();
-            play = false
-        } else {
+        if (!play) {
             audio_switchBGM.classList.remove('_pause');
             BGM.play();
-            play = true
+            play = true;
+        } else {
+            audio_switchBGM.classList.add('_pause');
+            
+            BGM.pause();
+            play = false;
         }
-    };
+    }
 
     let switchBGM = function() {
-        audio_switchBGM.addEventListener('click', toggleBGMBtn, false);
-    };
+        audio_switchBGM.addEventListener('click', toggleBGMBtn, false)
+    }
 
-    BGM.play();
+    // BGM.play();
 
     switchBGM();
 
